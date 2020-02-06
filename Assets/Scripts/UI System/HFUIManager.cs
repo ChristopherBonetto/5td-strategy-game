10f04f9db946cdbc9af4a73ff4f09dfe97b4b7bd
Pibo <paibotm@yahoo.it>
2020-02-06 20:51:26 +0100

    Check mouse over UI

--- Assets/Scripts/UI System/HFUIManager.cs
+++ Assets/Scripts/UI System/HFUIManager.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class HFUIManager : Singleton<HFUIManager>
 {
-    /// <summary>
-    /// Controls Collection.
-    /// Every key must provides only one value.
-    /// </summary>
-    public Dictionary<UIControlID, HFUIControl> controls = new Dictionary<UIControlID, HFUIControl>();
+	/* Components */
+
+	private UnityEngine.UI.GraphicRaycaster m_graphicRaycaster = null;
+
+	/// <summary>
+	/// Controls Collection.
+	/// Every key must provides only one value.
+	/// </summary>
+	public Dictionary<UIControlID, HFUIControl> controls = new Dictionary<UIControlID, HFUIControl>();
 
 
     #region Methods
+
+	void Awake()
+	{
+		m_graphicRaycaster = GetComponent<UnityEngine.UI.GraphicRaycaster>();
+	}
+
     /// <summary>
     /// Add new UIControl
     /// <see cref="UIControl"/>
@@ -77,5 +88,25 @@ public class HFUIManager : Singleton<HFUIManager>
         else
             throw new System.Exception("Control with " + id + " id, doesn't exist or it's not register.");
     }
-    #endregion
+
+	/// <summary>
+	/// Check if mouse is over a raycast target
+	/// </summary>
+	/// <returns>True if over a raycast target</returns>
+	public bool IsMouseOverUI()
+	{
+		if (m_graphicRaycaster)
+		{
+			UnityEngine.EventSystems.PointerEventData ped = new UnityEngine.EventSystems.PointerEventData(null);
+			ped.position = Input.mousePosition;
+			List<UnityEngine.EventSystems.RaycastResult> results = new List<UnityEngine.EventSystems.RaycastResult>();
+			m_graphicRaycaster.Raycast(ped, results);
+
+			return results.Count > 0;
+		}
+
+		return false;
+	}
+
+	#endregion
 }
