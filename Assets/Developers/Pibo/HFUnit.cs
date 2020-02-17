d1c6d5e3fea9f540b4a17d20a13b8c6bcd1a600f
AlessandroPantano <alessandro.pantano@outlook.it>
2020-02-18 00:48:37 +0100

    Fix event trigger in HFUnit

--- Assets/Developers/Pibo/HFUnit.cs
+++ Assets/Developers/Pibo/HFUnit.cs
@@ -164,11 +164,6 @@ namespace HF
 			HFEventManager.SubscribeTo<GameStates>(HFEventID.OnGameStateChanged, HandleGameStateChange);
 		}
 
-		void Start()
-		{
-			HFEventManager.TriggerEvent(HFEventID.OnRequestNewBehaviour, WaveSystem.RequestType.Pre);
-		}
-
 		void OnDisable()
 		{
 			HFEventManager.UnsubscribeFrom<GameStates>(HFEventID.OnGameStateChanged, HandleGameStateChange);
@@ -807,7 +802,7 @@ namespace HF
 			// #TEMP
 			transform.localScale = new Vector3(transform.localScale.x, 0.1f, transform.localScale.z);
 
-			HFEventManager.TriggerEvent(HFEventID.OnRequestNewBehaviour, this);
+			HFEventManager.TriggerEvent(HFEventID.OnUnitDeath, this);
 
 			if (m_baseStats.RewardCondition == HFRewardCondition.Kill)
 			{
