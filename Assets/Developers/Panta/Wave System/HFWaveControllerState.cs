0b302e91feeb46a012ce9daa9c80879e0f422fd3
AlessandroPantano <alessandro.pantano@outlook.it>
2020-02-24 17:03:30 +0100

    Add level to test (Level_01)

--- Assets/Developers/Panta/Wave System/HFWaveControllerState.cs
+++ Assets/Developers/Panta/Wave System/HFWaveControllerState.cs
@@ -7,8 +7,6 @@ namespace HF.WaveSystem
     public abstract class HFWaveControllerState
     {
         // Static instance of all states.
-        public static HFCheckInputState CheckingInput = new HFCheckInputState();
-        public static HFCheckAvailableMinorWaveState CheckingAvailableMinorWaves = new HFCheckAvailableMinorWaveState();
         public static HFCheckTimeElapsedState CheckingTimeElapsed = new HFCheckTimeElapsedState();
         public static HFExecuteBehaviourState ExecutingBehaviour = new HFExecuteBehaviourState();
 
