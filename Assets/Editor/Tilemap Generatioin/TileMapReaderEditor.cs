46c0831be2e2f39fc37214284270b76f8befb3d9
AlessandroPantano <alessandro.pantano@outlook.it>
2020-03-10 21:52:44 +0100

    Add new level editor / generation

---
+++ Assets/Editor/Tilemap Generatioin/TileMapReaderEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class TileMapReaderEditor : EditorWindow
+{
+    public TileMapReader m_tilemapReader;
+    public TileBase m_fillTile;
+
+    [MenuItem("GoodNorth/LevelEditor")]
+    public static void CreateWindow()
+    {
+        GetWindow<TileMapReaderEditor>("LevelEditor");
+    }
+
+    private void OnEnable()
+    {
+        m_tilemapReader = new TileMapReader();
+        m_tilemapReader.Grid = SceneView.FindObjectOfType<Grid>();
+    }
+
+    private void OnGUI()
+    {
+        Handles.BeginGUI();
+        {
+            GUIStyle style = new GUIStyle("box");
+
+            GUILayout.BeginVertical();
+            
+            GUILayout.Space(20);
+
+		    if (GUILayout.Button("Generate Map"))
+            {
+                m_tilemapReader.CheckParent();
+                m_tilemapReader.SetTileMapContainer();
+                m_tilemapReader.SetLayers();
+                m_tilemapReader.Spawn3DLevel();
+            }
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Destroy Map"))
+            {
+                m_tilemapReader.DestroyLevelGenerated();
+            }
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Push as prefab"))
+            {
+                m_tilemapReader.PushLevelAsPrefab(m_tilemapReader.m_LevelParent);
+            }
+            GUILayout.EndVertical();
+        }
+        Handles.EndGUI();
+    }
+}
---
+++ Assets/Scripts/Tiles/HFRuleTileCustom.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Human Factor/Tile Map/Rule Tile", fileName = "New_HF_RuleTile")]
+public class HFRuleTileCustom : RuleTile
+{
+    [Tooltip("Make visible or not the gameObject associated")]
+    public TileFlags m_flag;
+
+    /// <summary>
+    /// Store the Itilemap interface where it's drawn on.
+    /// </summary>
+    public ITilemap ITilemap { get; private set; }
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+		base.GetTileData(position, tilemap, ref tileData);
+		tileData.flags = m_flag;
+        ITilemap = tilemap;
+    }
+
+    public override bool StartUp(Vector3Int location, ITilemap tilemap, GameObject instantiatedGameObject)
+    {
+        bool value = base.StartUp(location, tilemap, instantiatedGameObject);
+
+        ITilemap = tilemap;
+        Matrix4x4 matrix = tilemap.GetTransformMatrix(location);
+        if (instantiatedGameObject != null)
+        {
+            GameObject go = instantiatedGameObject;
+            go.transform.rotation = matrix.rotation;
+        }
+
+        return value;
+    }
+}
