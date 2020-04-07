85d413532ce9622d7376ae0a90c3dbfc48ce9f4d
AlessandroPantano <alessandro.pantano@outlook.it>
2020-04-07 10:29:27 +0200

    Add refactoring wave system

---
+++ Assets/Developers/Panta/Refactoring/Wave System/Runtime/HFWaveBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HF.Refactoring
+{
+    public abstract class HFWaveBehaviour
+    {
+        public HFWaveBehaviour() { }
+
+
+        /// <summary>
+        /// Execute the behaviour
+        /// </summary>
+        public abstract void Execute(HFWaveController controller);
+
+
+        /// <summary>
+        /// Exit the behaviour and assign the next one.
+        /// </summary>
+        public abstract void Exit(HFWaveController controller);
+    }
+}
---
+++ Assets/Developers/Panta/Refactoring/Wave System/Runtime/HFWaveData.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HF;
+
+namespace HF.Refactoring
+{
+    public enum BehaviourType
+    {
+        SINGLE,
+        WAIT,
+        BULK
+    }
+
+    [CreateAssetMenu(fileName = "SO_wave_lvl_00", menuName = "Refactoring/Good North/Wave/New Wave Data")]
+    /// <summary>
+    /// Scriptable Object that collect behaviours of a single wave.
+    /// </summary>
+    public class HFWaveData : ScriptableObject
+    {
+        [SerializeField]
+        private List<HFWaveBehaviourData> m_behaviours = new List<HFWaveBehaviourData>(1);
+
+
+        [System.Serializable]
+        /// <summary>
+        /// Inner class to define a single behaviour of the wave.
+        /// </summary>
+        public class HFWaveBehaviourData
+        {
+            public BehaviourType Type = BehaviourType.SINGLE;
+            public HFBaseStats UnitData = null;
+            public int SpawnPointID = 0;
+            public float TimeToWait = 0;
+            public int AmountToSpawn = 0;
+        }
+
+
+        /// <summary>
+        /// Get all behaviours of the wave.
+        /// </summary>
+        public List<HFWaveBehaviourData> GetBehaviours()
+        {
+            return m_behaviours;
+        }
+
+
+        /// <summary>
+        /// Getthe count of enemies in this level.
+        /// </summary>
+        /// <returns></returns>
+        public int GetCountOfEnemies()
+        {
+            int valueToReturn = 0;
+
+            foreach (var behaviour in m_behaviours)
+            {
+                valueToReturn += behaviour.AmountToSpawn;
+            }
+
+            return valueToReturn;
+        }
+    }   
+}
