941a48abaa451a98968eb829b886a7a35e2e11d5
AlessandroPantano <alessandro.pantano@outlook.it>
2020-04-14 17:49:01 +0200

    Add flexible layout group, Add radial layout.

---
+++ Assets/Scripts/UI System/Runtime/Custom Layouts/HFRadialLayout.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HFRadialLayout : LayoutGroup
+{
+    public float Radius;
+
+    public override void CalculateLayoutInputVertical()
+    {
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            float angle = 360 / rectChildren.Count * i;
+            SetChildAlongAxis(rectChildren[i], 0, Radius * Mathf.Sin(angle * Mathf.Deg2Rad));
+            SetChildAlongAxis(rectChildren[i], 1, Radius * -Mathf.Cos(angle * Mathf.Deg2Rad));
+        }
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+    }
+
+    public override void SetLayoutVertical()
+    {
+    }
+}
