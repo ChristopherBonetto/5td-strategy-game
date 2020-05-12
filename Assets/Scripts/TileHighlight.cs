490470e9b5aac1cc66085cb6592491f040eeb998
FabioMellone <fabiomellone1993@live.it>
2020-05-12 15:47:46 +0200

    Added level 02

---
+++ Assets/Scripts/TileHighlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileHighlight : MonoBehaviour
+{
+    private Material startMaterial;
+    public Material highlightMaterial;
+    bool mouseOver = false;
+
+    private void Awake()
+    {
+        startMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOver = true;
+        GetComponent<Renderer>().material = highlightMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+        GetComponent<Renderer>().material = startMaterial;
+    }
+
+}
