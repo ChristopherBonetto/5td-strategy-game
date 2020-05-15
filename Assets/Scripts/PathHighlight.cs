b5b34b55804e6860741677367eef9c9d1d8fede7
FabioMellone <fabiomellone1993@live.it>
2020-05-15 17:35:24 +0200

    Added path highlight (to be updated)

--- Assets/Scripts/PathHighlight.cs
+++ Assets/Scripts/PathHighlight.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(LineRenderer))]
 
 public class PathHighlight : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private LineRenderer myLineRenderer;
+    private NavMeshAgent myNavMeshAgent;
+
+
+
+
     void Start()
     {
-        
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        myLineRenderer = GetComponent<LineRenderer>();
+
+        myLineRenderer.startWidth = 0.15f;
+        myLineRenderer.endWidth = 0.15f;
+        myLineRenderer.positionCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        if (myNavMeshAgent.hasPath)
+        {
+            DrawPath();
+        }
+    }
+    void DrawPath()
+    {
+        myLineRenderer.positionCount = myNavMeshAgent.path.corners.Length;
+        myLineRenderer.SetPosition(0, transform.position);
+
+     
         
+       
+       
+
+
+        if (myNavMeshAgent.path.corners.Length < 2)
+        {
+            return;
+        }
+
+        for(int i=1;i<myNavMeshAgent.path.corners.Length;i++)
+        {
+            Vector3 pointPosition = new Vector3(myNavMeshAgent.path.corners[i].x, myNavMeshAgent.path.corners[i].y, myNavMeshAgent.path.corners[i].z);
+            myLineRenderer.SetPosition(i, pointPosition);
+         
+        }
+            
     }
 }
--- Assets/Scripts/TileHighlight.cs
+++ Assets/Scripts/TileHighlight.cs
@@ -41,7 +41,7 @@ public class TileHighlight : MonoBehaviour
     {
         transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
         yield return new WaitForSeconds(0.2f);
-        gameObject.SetActive(false);
+        
     }
 
   
