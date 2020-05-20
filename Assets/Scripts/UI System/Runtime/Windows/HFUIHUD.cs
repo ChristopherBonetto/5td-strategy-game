74b8f1010cffafbcb30e8206fb84db9876be4977
AlessandroPantano <alessandro.pantano@outlook.it>
2020-05-20 16:53:29 +0200

    Delete specialization and upgrades in UI

--- Assets/Scripts/UI System/Runtime/Windows/HFUIHUD.cs
+++ Assets/Scripts/UI System/Runtime/Windows/HFUIHUD.cs
@@ -20,17 +20,10 @@ namespace HF.Refactoring
         public HFButton ButtonCallNextWave;
 
 
-        [Header("Pop-ups")]
-        public EntityUpgradeButton EntityUpgradeButton;
-        public EntitySpecializeButton EntitySpecializeButton;
-
-
         private void OnEnable()
         {
             HFEventManager.SubscribeTo(HFEventID.OnWaveBeginned, OnNewWaveBegin);
             HFEventManager.SubscribeTo(HFEventID.OnWaveCleared, OnWaveCleared);
-            HFEventManager.SubscribeTo<EntityBehavior, int>(HFEventID.OnUnitSelected, OnunitSelected);
-            HFEventManager.SubscribeTo(HFEventID.OnUnitLift, OnUnitLift);
 
             ButtonCallNextWave.gameObject.SetActive(true);
         }
@@ -39,8 +32,6 @@ namespace HF.Refactoring
         {
             HFEventManager.UnsubscribeFrom(HFEventID.OnWaveBeginned, OnNewWaveBegin);
             HFEventManager.UnsubscribeFrom(HFEventID.OnWaveCleared, OnWaveCleared);
-            HFEventManager.UnsubscribeFrom<EntityBehavior, int>(HFEventID.OnUnitSelected, OnunitSelected);
-            HFEventManager.UnsubscribeFrom(HFEventID.OnUnitLift, OnUnitLift);
         }
 
         #region Events
@@ -73,55 +64,6 @@ namespace HF.Refactoring
             HFScenesManager.Instance.EndCurrentLevel(true);
             HFUIManager.Instance.ShowAndClearHistory(HFUIWindowID.WAR_ROOM);
             HFScenesManager.Instance.LoadSceneFromIndex(1);
-        }
-
-        public void OnUnitLift()
-        {
-            EntitySpecializeButton.gameObject.SetActive(false);
-            EntityUpgradeButton.gameObject.SetActive(false);
-        }
-
-        public void OnunitSelected(EntityBehavior entity, int team)
-        {
-            if (entity != null)
-            {
-                EntitySpecializeButton.gameObject.SetActive(false);
-                EntityUpgradeButton.gameObject.SetActive(false);
-
-
-                // The case it's a unit
-                if (entity is Troop)
-                {
-                    if (entity.IsBusy)
-                    {
-                        // Trigger an event that display "Unit can't be upgraded"
-                        return;
-                    }
-                    if ((entity as Troop).BuildingHandled != null)
-                    {
-                        // Trigger an event that display "Unit can't be upgraded"
-                        return;
-                    }
-
-                    UnitsStatsSO stats = entity.GetComponent<Troop>().GetStats();
-
-                    if (stats.UnitType != Types.UnitType.PEASANT)
-                        EntityUpgradeButton.SetUpgradeButton(entity);
-                    else if (stats.UnitType == Types.UnitType.PEASANT)
-                        EntitySpecializeButton.SetSpecializeButton(entity);
-                }
-                // the case it's a building
-                else if (entity is BuildingBehaviour)
-                {
-                    BuildingsStatsSO stats = entity.GetComponent<BuildingBehaviour>().GetStats();
-                    EntityUpgradeButton.SetUpgradeButton(entity);
-                }
-            }
-            else
-            {
-                EntitySpecializeButton.gameObject.SetActive(false);
-                EntityUpgradeButton.gameObject.SetActive(false);
-            }
         }
     }
 }
