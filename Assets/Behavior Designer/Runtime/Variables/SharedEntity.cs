48f6aaf71f55977a0fd2a325d1be934521e239f7
AlessandroPantano <alessandro.pantano@outlook.it>
2020-05-27 16:57:19 +0200

    Create shared variable, type --> EntityBehaviour

--- Assets/Behavior Designer/Runtime/Variables/SharedEntity.cs
+++ Assets/Behavior Designer/Runtime/Variables/SharedEntity.cs
@@ -3,6 +3,6 @@
     [System.Serializable]
     public class SharedEntity : SharedVariable<EntityBehavior>
     {
-        public static implicit operator SharedEntity(EntityBehavior value) { return new SharedEntity { mValue = value }; }
+        public static implicit operator SharedEntity(EntityBehavior value) { return new SharedEntity { Value = value }; }
     }
 }
