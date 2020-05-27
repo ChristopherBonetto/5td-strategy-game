f632e7e4b32979ac4293cac078970ce379304e0d
Christopher.Bonetto <christopherbonetto@gmail.com>
2020-05-27 17:10:35 +0200

    new scenes

--- Assets/Scripts/GameplayScripts/Entities/BuildingScripts/CastleStarter.cs
+++ Assets/Scripts/GameplayScripts/Entities/BuildingScripts/CastleStarter.cs
@@ -6,8 +6,9 @@ using BehaviorDesigner.Runtime;
 
 public class CastleStarter : BuildingBehaviour
 {
-    [SerializeField]
-    private Transform[] m_spawnPoints;
+    [SerializeField] private Transform[] m_towerSpawnPoints;
+
+    [SerializeField] private Transform m_unitSpawnPoint;
 
     // Start is called before the first frame update
     public override void Start()
@@ -19,19 +20,21 @@ public class CastleStarter : BuildingBehaviour
 
         // Set this castel as reference in the bh tree.
         GlobalVariables.Instance.SetVariableValue("Castle", this.gameObject);
+
+        if (m_towerSpawnPoints == null) return;
+        foreach (Transform t in m_towerSpawnPoints)
+        {
+            GameController.Instance.CreateNewBuilding(BuildingType.TOWER, PlayerType.Player, t.transform.position.SnapLocation());
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (m_spawnPoints == null) return;
+            if (m_unitSpawnPoint == null) return;
 
-            foreach (Transform t in m_spawnPoints)
-            {
-                GameController.Instance.CreateNewTroop(UnitType.STANDARD_ALLY, PlayerType.Player, t.position.SnapLocation());
-            }
-            GameController.Instance.CreateNewBuilding(BuildingType.TOWER, PlayerType.Player, (new Vector3(transform.position.x + 5, transform.position.y, transform.position.z + 5).SnapLocation()));
+            GameController.Instance.CreateNewTroop(UnitType.STANDARD_ALLY, PlayerType.Player, m_unitSpawnPoint.position.SnapLocation());
         }
     }
 }
