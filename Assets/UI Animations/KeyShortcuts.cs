6ca4e7448127783d274f979c3eedc015adb89a1d
FabioMellone <fabiomellone1993@live.it>
2020-06-09 18:17:37 +0200

    Added key shortcuts

---
+++ Assets/UI Animations/KeyShortcuts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class KeyShortcuts : MonoBehaviour
+{
+    public KeyCode assignedKey;
+
+    public Button m_Button;
+
+    void Update()
+    {
+        if( m_Button!=null)
+        {
+            if(Input.GetKeyDown(assignedKey))
+            {
+                m_Button.onClick.Invoke();
+            }
+        }
+    }
+}
