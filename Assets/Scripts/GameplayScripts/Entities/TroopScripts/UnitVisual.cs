a6baefad4412d2f01b2c48004c75b17fa20fca8f
Christopher.Bonetto <christopherbonetto@gmail.com>
2020-06-10 16:11:10 +0200

    fabio voleva il sangue

---
+++ Assets/Scripts/GameplayScripts/Entities/TroopScripts/UnitVisual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitVisual : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem m_takeDamageEffect;
+    public ParticleSystem TakeDamageEffect { get => m_takeDamageEffect; }
+
+    public void PlayParticle(ParticleSystem inPart)
+    {
+        inPart.gameObject.SetActive(true);
+        inPart.Play();
+    }
+
+    public void StopParticle(ParticleSystem inPart)
+    {
+        inPart.Stop();
+        inPart.gameObject.SetActive(false);
+    }
+}
