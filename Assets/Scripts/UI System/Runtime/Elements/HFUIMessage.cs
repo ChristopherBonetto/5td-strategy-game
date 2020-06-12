2c471075e48665e9c26b0e464bb6284b0f7e8e43
AlessandroPantano <alessandro.pantano@outlook.it>
2020-06-12 12:13:41 +0200

    ADd message component

---
+++ Assets/Scripts/UI System/Runtime/Elements/HFUIMessage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HFUIMessage : MonoBehaviour
+{
+    public Text TextComponent;
+    public float FadeDuration = 2;
+
+    public void SetMessage(string message)
+    {
+        TextComponent.text = message;
+        gameObject.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        TextComponent.DOFade(1, FadeDuration).
+            OnComplete(() => TextComponent.DOFade(0, FadeDuration)).
+            OnComplete(() => gameObject.SetActive(false));
+    }
+
+    private void OnDisable()
+    {
+        TextComponent.text = "";
+    }
+}
