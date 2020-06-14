510378de68c276f6b1496fec85d7a1b271bec75c
FabioMellone <fabiomellone1993@live.it>
2020-06-14 16:16:29 +0200

    Added ragdoll corpses for lifters and base ally/enemy

---
+++ Assets/VFX/VFX Prefabs/Corpses/CorpsesMassRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorpsesMassRandomizer : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+    private float mass;
+    public float minMultiplier = .6f;
+    public float maxMultiplier = 1.4f;
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        mass = Random.Range(m_rigidbody.mass * minMultiplier, m_rigidbody.mass * maxMultiplier);
+        m_rigidbody.mass = mass;
+        m_rigidbody.AddForce(4f*Random.Range(m_rigidbody.mass * minMultiplier, m_rigidbody.mass * maxMultiplier), 4f*Random.Range(m_rigidbody.mass * minMultiplier, m_rigidbody.mass * maxMultiplier), 4f*Random.Range(m_rigidbody.mass * minMultiplier, m_rigidbody.mass * maxMultiplier), ForceMode.Impulse);
+    }
+}
