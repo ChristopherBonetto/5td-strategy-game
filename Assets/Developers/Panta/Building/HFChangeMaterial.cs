dac238bf3f53f99b0972260de96b2c3fb3140abb
AlessandroPantano <alessandro.pantano@outlook.it>
2020-07-01 15:21:51 +0200

    Update tutorial system

---
+++ Assets/Developers/Panta/Building/HFChangeMaterial.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HFChangeMaterial : MonoBehaviour, IHFTutorial
+{
+    public GameEventData Event;
+    public Material NewMaterial;
+    public Material DefaultMaterial;
+
+    public bool UseSkinnedMesh = false;
+    public MeshRenderer Mesh;
+    public SkinnedMeshRenderer SkinnedMesh;
+
+    private void OnEnable()
+    {
+        Event.AddListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.RemoveListener(this);
+    }
+
+
+    private TutorialID m_TutorialID = TutorialID.Select_Castle;
+    public TutorialID TutorialID { get => m_TutorialID; set => m_TutorialID = value; }
+
+    public void OnGlobalInitialization()
+    {
+    }
+
+    public void OnStepCompleted()
+    {
+
+        if (UseSkinnedMesh)
+            SkinnedMesh.material = DefaultMaterial;
+        else
+            Mesh.material = DefaultMaterial;
+    }
+
+    public void OnStepInitialization()
+    {
+        if (UseSkinnedMesh)
+            SkinnedMesh.material = NewMaterial;
+        else
+            Mesh.material = NewMaterial;
+    }
+
+    public void Reset()
+    {
+        if (UseSkinnedMesh)
+            SkinnedMesh.material = DefaultMaterial;
+        else
+            Mesh.material = DefaultMaterial;
+    }
+}
---
+++ Assets/Developers/Panta/Building/HFEnableModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HFEnableModel : MonoBehaviour, IHFTutorial
+{
+    public GameEventData Event;
+    public GameObject TutorialModel;
+    public GameObject DefaultModel;
+
+
+    private void OnEnable()
+    {
+        Event.AddListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.RemoveListener(this);
+    }
+
+
+    private TutorialID m_TutorialID = TutorialID.Select_Castle;
+    public TutorialID TutorialID { get => m_TutorialID; set => m_TutorialID = value; }
+
+    public void OnGlobalInitialization()
+    {
+        TutorialModel.SetActive(false);
+    }
+
+    public void OnStepCompleted()
+    {
+        TutorialModel.SetActive(false);
+    }
+
+    public void OnStepInitialization()
+    {
+        TutorialModel.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        TutorialModel.SetActive(false);
+    }
+}
