b5fc712c2b861af811498730c01e38092c08db05
FabioMellone <fabiomellone1993@live.it>
2020-07-02 17:18:24 +0200

    updated game scenes

--- Assets/Scripts/GameplayScripts/GemGenerator.cs
+++ Assets/Scripts/GameplayScripts/GemGenerator.cs
@@ -3,63 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GemGenerator : MonoBehaviour, IClickable
+public class GemGenerator : MonoBehaviour
 {
     [SerializeField] private Transform m_destinationPoint;
     [SerializeField] private float m_xOffSet = 1;
     [SerializeField] private float m_zOffSet = 1;
+    [SerializeField] private float timer = 10f;
+    [SerializeField] private float currentTimer = 0;
+    bool canSpawnGem = false;
+
+
 
     private void OnEnable()
     {
-        HFEventManager.SubscribeTo(HFEventID.OnWaveBeginned, StartTimer);
-        HFEventManager.SubscribeTo(HFEventID.OnWaveCleared, StopSpawn);
+        HFEventManager.SubscribeTo(HFEventID.OnWaveBeginned, ActivateGemSpawn);
+        HFEventManager.SubscribeTo(HFEventID.OnWaveCleared, StopGemSpawn);
     }
     private void OnDisable()
     {
-        HFEventManager.UnsubscribeFrom(HFEventID.OnWaveBeginned, StartTimer);
-        HFEventManager.UnsubscribeFrom(HFEventID.OnWaveCleared, StopSpawn);
+        HFEventManager.UnsubscribeFrom(HFEventID.OnWaveBeginned, ActivateGemSpawn);
+        HFEventManager.UnsubscribeFrom(HFEventID.OnWaveCleared, StopGemSpawn);
     }
 
-    public void StartTimer()
+    private void Update()
     {
-        StartCoroutine(SpawnGemCoroutine());
-    }
-
-    IEnumerator SpawnGemCoroutine()
-    {
-        yield return new WaitForSeconds(3f);
-
         SpawnGem();
-
-        StartCoroutine(SpawnGemCoroutine());
     }
+
+
 
     public void SpawnGem()
     {
-        GameObject gem = ObjectPooler.Instance.GetPooledObject("Gem");
-        gem.transform.position = this.transform.position;
-        gem.SetActive(true);
-
-        float x = Random.Range(-m_xOffSet, m_xOffSet);
-        float z = Random.Range(-m_zOffSet, m_zOffSet);
-
-        Vector3 destination = m_destinationPoint.transform.position + new Vector3(x, 0, z);
-
-        gem.transform.DOJump(destination, 5, 2, 1f);
-    }
-
-    public void StopSpawn()
-    {
-        StopAllCoroutines();
-    }
-
-    public void Click()
-    {
-        SpawnGem();
-    }
-
-    public void Deselected()
-    {
         
+      if(canSpawnGem==true)
+        {
+            if (currentTimer >= timer)
+            {
+
+                currentTimer = 0;
+                GameObject gem = ObjectPooler.Instance.GetPooledObject("Gem");
+                gem.transform.position = this.transform.position;
+                gem.SetActive(true);
+
+                float x = Random.Range(-m_xOffSet, m_xOffSet);
+                float z = Random.Range(-m_zOffSet, m_zOffSet);
+
+                Vector3 destination = m_destinationPoint.transform.position + new Vector3(x, 0, z);
+
+                gem.transform.DOJump(destination, 5, 2, 1f);
+            }
+            else currentTimer += Time.deltaTime;
+        }
     }
+
+
+
+    public void ActivateGemSpawn()
+    {
+        canSpawnGem = true;
+    }
+    public void StopGemSpawn()
+    {
+        canSpawnGem = false;
+    }
+
+
+
+
 }
