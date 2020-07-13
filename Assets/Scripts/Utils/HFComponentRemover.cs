5c16facb7c84fbda717114d5f05c3a9126d5175e
AlessandroPantano <alessandro.pantano@outlook.it>
2020-07-13 12:19:15 +0200

    Clear levels from 1 to 6, Remove all useless components

---
+++ Assets/Scripts/Utils/HFComponentRemover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HFComponentRemover : MonoBehaviour
+{
+    public enum ComponentTypes 
+    {
+        Collider,
+        NavMeshObstacle,
+        TileHighLight,
+        SpriteRenderer,
+    }
+
+    public ComponentTypes TypeToRemove;
+
+    [ContextMenu("Remove Components in childrens")]
+    public void RemoveComponentAction() 
+    {
+        switch (TypeToRemove) {
+            case ComponentTypes.Collider:
+                RemoveComponent<Collider>();
+                break;
+            case ComponentTypes.NavMeshObstacle:
+                RemoveComponent<NavMeshObstacle>();
+                break;
+            case ComponentTypes.TileHighLight:
+                RemoveComponent<TileHighlight>();
+                break;
+            case ComponentTypes.SpriteRenderer:
+                RemoveComponent<SpriteRenderer>();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void RemoveComponent<T>() where T : Component
+    {
+        T[] obstacles;
+        obstacles = GetComponentsInChildren<T>();
+
+        foreach (var item in obstacles) 
+        {
+            DestroyImmediate(item);
+        }
+    }
+}
