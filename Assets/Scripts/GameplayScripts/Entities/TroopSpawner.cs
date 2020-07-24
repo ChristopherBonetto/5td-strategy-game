98fa73f200cc84b74943664fb8e998f85b65c725
Christopher.Bonetto <christopherbonetto@gmail.com>
2020-07-24 16:34:12 +0200

    entities spwner

---
+++ Assets/Scripts/GameplayScripts/Entities/TroopSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Types;
+
+public class TroopSpawner : MonoBehaviour
+{
+    [SerializeField] private UnitType unitType;
+    [SerializeField] private PlayerType playerType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.Instance.CreateNewTroop(unitType, playerType, transform.position, true);
+    }
+}
